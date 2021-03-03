e382405dbebd7a953e51b222099684b577e2df22
cfolliet <cfolliet@talentsoft.com>
2021-03-03 17:15:11 +0100

    do not display charts for epic we are not working on

--- src/components/Epic.svelte
+++ src/components/Epic.svelte
@@ -2,6 +2,43 @@
 	import PastChart from "../components/PastChart.svelte";
 	import FutureChart from "../components/FutureChart.svelte";
 	export let data;
+
+	let hasPastData = false;
+	const pastChartData = Object.entries(data.past).map(([week, issues]) => {
+		if (issues.length) {
+			hasPastData = true;
+		}
+		return { x: week, y: issues.length };
+	});
+
+	const futureChartData = !data.future
+		? []
+		: data.future.reduce(
+				(acc, value) => {
+					if (
+						acc.cumulative[acc.cumulative.length - 1] &&
+						acc.cumulative[acc.cumulative.length - 1].y >= 99
+					) {
+						return acc;
+					}
+					acc.percent.push(value);
+					const lastCumulative = acc.cumulative[
+						acc.cumulative.length - 1
+					]
+						? acc.cumulative[acc.cumulative.length - 1].y
+						: 0;
+
+					acc.cumulative.push({
+						x: value.x,
+						y: value.y + lastCumulative,
+					});
+					return acc;
+				},
+				{
+					percent: [],
+					cumulative: [],
+				}
+		  );
 </script>
 
 <section>
@@ -13,45 +50,12 @@
 		</h3>
 		<small>Reste à faire: {data.unresolved} stories</small>
 		<p>
-			<PastChart
-				id={"past" + data.epic}
-				data={Object.entries(data.past).map(([week, issues]) => {
-					return { x: week, y: issues.length };
-				})}
-			/>
-			<FutureChart
-				id={"future" + data.epic}
-				data={!data.future
-					? []
-					: data.future.reduce(
-							(acc, value) => {
-								if (
-									acc.cumulative[acc.cumulative.length - 1] &&
-									acc.cumulative[acc.cumulative.length - 1]
-										.y >= 99
-								) {
-									return acc;
-								}
-								acc.percent.push(value);
-								const lastCumulative = acc.cumulative[
-									acc.cumulative.length - 1
-								]
-									? acc.cumulative[acc.cumulative.length - 1]
-											.y
-									: 0;
-
-								acc.cumulative.push({
-									x: value.x,
-									y: value.y + lastCumulative,
-								});
-								return acc;
-							},
-							{
-								percent: [],
-								cumulative: [],
-							}
-					  )}
-			/>
+			{#if hasPastData}
+				<PastChart id={"past" + data.epic} data={pastChartData} />
+				<FutureChart id={"future" + data.epic} data={futureChartData} />
+			{:else}
+				Aucun progrès en cours
+			{/if}
 		</p>
 	</aside>
 </section>
